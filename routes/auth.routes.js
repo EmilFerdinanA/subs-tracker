dc8fbc6e3ce849e8a72d35c5b655029b86550eb4
EmilFerdinanA <emilakbar55@gmail.com>
2025-06-11 22:18:47 +0700

    create user with transaction

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controllers.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) =>
-  res.status(200).json({ message: "sign up" })
-);
-
-authRouter.post("/sign-in", (req, res) =>
-  res.status(200).json({ message: "sign in" })
-);
-
-authRouter.post("/sign-out", (req, res) =>
-  res.status(200).json({ message: "sign out" })
-);
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
